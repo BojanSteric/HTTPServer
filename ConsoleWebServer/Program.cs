037eff84277f58879b0820aedd959817dcf61472
BojanSteric <bojansteric7@gmail.com>
2024-12-19 00:12:29 +0100

    initial console app and server with simple logging

---
+++ ConsoleWebServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using MyHttpServer;
+using System;
+
+
+namespace ConsoleWebServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Server.Start();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WebServer/Server.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyHttpServer
+{
+    public class Server
+    {
+        public static int maxSimultaneousConnections = 20;
+        private static Semaphore sem = new Semaphore(maxSimultaneousConnections, maxSimultaneousConnections);
+
+        private static List<IPAddress> GetLocalHostIPs()
+        {
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            List<IPAddress> ret = host.AddressList.Where(ip => ip.AddressFamily == AddressFamily.InterNetwork).ToList();
+
+            return ret;
+        }
+
+        private static HttpListener InitializeListener(List<IPAddress> localhostIPs)
+        {
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost/");
+
+            // Listen to IP address as well.
+            localhostIPs.ForEach(ip =>
+            {
+                Console.WriteLine("Listening on IP " + "http://" + ip.ToString() + "/");
+                listener.Prefixes.Add("http://" + ip.ToString() + "/");
+            });
+
+            return listener;
+        }
+
+        //Actual Start
+        private static void Start(HttpListener listener)
+        {
+            listener.Start();
+            Task.Run(() => RunServer(listener));
+        }
+
+
+        private static void RunServer(HttpListener listener)
+        {
+            while (true)
+            {
+                sem.WaitOne();
+                StartConnectionListener(listener);
+            }
+        }
+
+        private static async void StartConnectionListener(HttpListener listener)
+        {
+            // Wait for a connection. Return to caller while we wait.
+            HttpListenerContext context = await listener.GetContextAsync();
+
+            // Release the semaphore so that another listener can be immediately started up.
+            sem.Release();
+            
+            Log(context.Request);
+
+            string response = "<html><head><meta http-equiv='content-type' content='text/html; charset=utf-8'/></head> Hello Browser! </ html >";
+            byte[] encoded = Encoding.UTF8.GetBytes(response);
+            context.Response.ContentLength64 = encoded.Length;
+            context.Response.OutputStream.Write(encoded, 0, encoded.Length);
+            context.Response.OutputStream.Close();
+        }
+
+        public static void Log(HttpListenerRequest request)
+        {
+            Console.WriteLine(request.RemoteEndPoint + " " + request.HttpMethod + " /" + request.Url.AbsoluteUri);
+        }
+
+        //Public start
+        public static void Start()
+        {
+            List<IPAddress> localHostIPs = GetLocalHostIPs();
+            HttpListener listener = InitializeListener(localHostIPs);
+            Start(listener);
+        }
+
+    }
+}
